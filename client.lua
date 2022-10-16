5552dbfcb2363555573d8eff37e445b35f11e413
Exo-Scripts <115940454+Exo-Scripts@users.noreply.github.com>
2022-10-16 21:41:04 +0300

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,46 @@
+--================================--
+--      exo_safezones 1.0      --
+--      by exo | scripts           --
+--		GNU License v3.0		  --
+--================================--
+
+CreateThread(function()
+    while true do
+        playerCoords = GetEntityCoords(PlayerPedId()) -- Gets player coords every 5th of a second
+        Wait(500)
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1)
+        local locations = Config.Locations
+        for i = 1, #locations do
+            local x = locations[i][1];
+            local y = locations[i][2];
+            local z = locations[i][3];
+            local radius = Config.Radius
+            if #(playerCoords - vector3(x, y, z)) <= radius then
+                DisplayHelpText(Config.Alert) 
+                if IsControlJustPressed(0, 106) then
+                    ShowInfo(Config.Warning)
+                end
+                SetPlayerCanDoDriveBy(player, false)
+                DisablePlayerFiring(player, true)
+                DisableControlAction(0, 140)
+            end
+        end
+    end
+end)
+
+function DisplayHelpText(text)
+    SetTextComponentFormat("STRING")
+    AddTextComponentString(text)
+    DisplayHelpTextFromStringLabel(0, 0, 0, -1)
+end
+
+function ShowInfo(text)
+    SetNotificationTextEntry("STRING")
+	AddTextComponentString(text)
+	DrawNotification(true, false)
+end
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+author "Exo | Scripts"
+description "Safezones for FiveM"
+version "1.0"
+
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+shared_script "config.lua"
+client_script "source/client.lua"
